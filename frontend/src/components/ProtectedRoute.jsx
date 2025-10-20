20b6febe63344aad9eb6ed087d11a403bb2067bd
draohenrick <agenciablocktool@gmail.com>
2025-10-20 15:34:18 -0300

    Create ProtectedRoute.jsx

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = ({ children, roles }) => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" />;
+  if (roles && !roles.includes(user.type)) return <Navigate to="/login" />;
+  return children;
+};
+
+export default ProtectedRoute;
