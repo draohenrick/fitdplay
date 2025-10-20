589f06964b600b9d3eef047dd9d510b0dbf2954c
draohenrick <agenciablocktool@gmail.com>
2025-10-19 22:27:43 -0300

    Update Input.jsx

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,13 +1,14 @@
-export default function Input({ label, type, value, onChange, placeholder }) {
+export default function Input({ label, type="text", name, value, onChange, placeholder }) {
   return (
     <div className="flex flex-col mb-4">
-      <label className="mb-1 font-semibold text-gray-700">{label}</label>
+      <label className="mb-1 font-medium text-gray-700">{label}</label>
       <input
         type={type}
+        name={name}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-600"
+        className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
   );
