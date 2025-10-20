d8e54e75994fabeacada78999f8f805b019bf278
draohenrick <agenciablocktool@gmail.com>
2025-10-19 22:58:53 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,34 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css"; // Importa o Tailwind CSS
 
-// Importações de componentes e páginas
-import Layout from "./components/Layout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import Perfil from "./pages/Perfil";
-
-// Definição das rotas
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/dashboard", element: <Dashboard /> },
-      { path: "/perfil", element: <Perfil /> },
-      { path: "/login", element: <Login /> },
-      { path: "/register", element: <Register /> },
-    ],
-  },
-]);
-
-// Renderização da aplicação
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
