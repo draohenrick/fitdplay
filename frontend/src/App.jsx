fe8241d4deaa56c9d864917c4f64f959167eea3e
draohenrick <agenciablocktool@gmail.com>
2025-10-20 09:29:13 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import DashboardGerente from "./pages/DashboardGerente";
-import DashboardVendedor from "./pages/DashboardVendedor";
-import DashboardLoja from "./pages/DashboardLoja";
-
-export default function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/gerente" element={<DashboardGerente />} />
-        <Route path="/vendedor" element={<DashboardVendedor />} />
-        <Route path="/loja" element={<DashboardLoja />} />
-      </Routes>
-    </Router>
-  );
-}
+import DashboardGerente from './pages/DashboardGerente';
+import DashboardVendedor from './pages/DashboardVendedor';
+import DashboardLoja from './pages/DashboardLoja';
+import Clientes from './pages/Clientes';
+import Pedidos from './pages/Pedidos';
+import Produtos from './pages/Produtos';
+import Perfil from './pages/Perfil';
