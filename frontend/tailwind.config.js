9354b3bfff6af232f42d25f44a77e968ceb5b1aa
draohenrick <agenciablocktool@gmail.com>
2025-10-20 08:39:16 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#FF0054",
-        secondary: "#2E2E2E",
-        accent: "#FFD300",
-      },
-    },
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
